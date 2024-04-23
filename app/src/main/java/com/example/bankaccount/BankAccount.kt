b97afd053b3530aa79578f2a2d91c5edebac8a89
Dimuello <phoenix.design.room@gmail.com>
2024-04-23 22:22:20 +0300

    Create BankAcoount class

---
+++ app/src/main/java/com/example/bankaccount/BankAccount.kt
@@ -0,0 +1,11 @@
+package com.example.bankaccount
+
+class BankAccount(val accountHolder: String, var balance: String) {
+
+    private val transactionHistory = mutableListOf<String>()
+
+    fun deposit(amount: Double) {
+
+    }
+
+}
